c9523d09fbc8d966d3c0328ad52cbfd9d5e57187
root <root@renauddha.ovh>
2020-03-04 17:26:47 +0100

    adding docker and docker-compose with their respective config files

---
+++ src/nextcloud/app/https.config.php
@@ -0,0 +1,9 @@
+<?php
+$CONFIG = array (
+  'trusted_domains' => 
+  array (
+    0 => 'nextcloud.<mydomain.com>',
+  ),
+  'overwriteprotocol' => 'https',  
+  'overwrite.cli.url' => 'http://nextcloud.<mydomain.com>',
+);
---
+++ src/nextcloud/app/mail.config.php
@@ -0,0 +1,14 @@
+<?php
+$CONFIG = array (
+  'mail_smtpmode' => 'smtp',
+  'mail_smtpsecure' => 'ssl',
+  'mail_sendmailmode' => 'smtp',
+  'mail_from_address' => '<adress>',
+  'mail_domain' => '<domain>',
+  'mail_smtpauthtype' => 'LOGIN',
+  'mail_smtpauth' => 1,
+  'mail_smtphost' => '<smtp_host>',
+  'mail_smtpport' => '465',
+  'mail_smtpname' => '<email>',
+  'mail_smtppassword' => '<smtp_password>',
+);
